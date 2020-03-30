93a67b2b64443f87b0e6888e1df5771206125e2c
Peter van Heusden <pvh@sanbi.ac.za>
2020-03-30 08:35:08 +0200

    Add GeoJSON, dependencies

---
+++ src/components/Map.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <l-map
+                    ref="map"
+                    id="map"
+                    style="height: 600px; width: 100%"
+                    :zoom="zoom"
+                    :extraOptions="extraOptions"
+                    :center="center">
+                    <l-tile-layer :url="url" :attribution="attribution"/>
+                </l-map>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    import { LMap, LTileLayer, L } from "vue2-leaflet";
+
+    export default {
+        name: "Map",
+        data() {
+            return {
+                url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors\'',
+                extraOptions: { zoomControl: false },
+                zoom:
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
