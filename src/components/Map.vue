5b24e6d55c2ded60c0d08e2479a8d6cff16630e6
Peter van Heusden <pvh@sanbi.ac.za>
2020-03-30 09:24:47 +0200

    Replace json with geojson

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,32 +2,51 @@
     <v-container>
         <v-row>
             <v-col cols="12">
-                <l-map
-                    ref="map"
-                    id="map"
-                    style="height: 600px; width: 100%"
-                    :zoom="zoom"
-                    :extraOptions="extraOptions"
-                    :center="center">
-                    <l-tile-layer :url="url" :attribution="attribution"/>
-                </l-map>
+                <div class="map">
+                    <l-map
+                            ref="map"
+                            id="map"
+                            style="height: 600px; width: 100%"
+                            :zoom="zoom"
+                            :extraOptions="extraOptions"
+                            :center="center">
+                        <l-tile-layer v-if="url" :url="url" :attribution="attribution"/>
+                    </l-map>
+                </div>
             </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script>
-    import { LMap, LTileLayer, L } from "vue2-leaflet";
+    import axios from 'axios';
+    import L from 'leaflet';
+    import { LMap, LTileLayer } from "vue2-leaflet"
 
     export default {
         name: "Map",
         data() {
             return {
-                url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                url: null,
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors\'',
                 extraOptions: { zoomControl: false },
-                zoom:
+                zoom: 16,
+                center: L.latLng(-34.0956, 18.4840),
+                features: null,
             }
+        },
+        components: {
+            LMap,
+            LTileLayer
+        },
+        mounted() {
+            let tile_base = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+            this.url = tile_base;
+
+            let data_url = 'https://raw.githubusercontent.com/pvanheus/can-street-map/master/data/StreetsOfMuizenberg.json';
+            axios.get(data_url).then(response => {
+                this.feature = response.data
+            })
         }
     }
 </script>
